7f0c8b1f443b419b56eaa0e701593c733a6f7a6a
biro23 <121694408+biro23@users.noreply.github.com>
2024-01-05 10:41:51 -0300

    new project

---
+++ main.js
@@ -0,0 +1,24 @@
+// consumindo uma api do github
+
+document.addEventListener("DOMContentLoaded", function () {
+  const nameElement = document.querySelector("#name");
+  const usernameElement = document.querySelector("#username");
+  const avatarElement = document.querySelector("#avatar");
+  const followersElement = document.querySelector("#followers");
+  const followingElement = document.querySelector("following");
+  const linkElement = document.querySelector("#link");
+
+  fetch("https://api.github.com/users/biro23?tab=repositories")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (json) {
+      nameElement.innerText = json.name;
+      usernameElement.innerText = json.login;
+      avatarElement.src = json.avatar_url;
+      followingElement.innerText = json.following;
+      followersElement.innerText = json.followers;
+      ReportingObserver.innerText = json.public_repos;
+      linkElement.href = json.html_url;
+    });
+});
